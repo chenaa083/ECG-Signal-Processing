63d914619bdefeaa959f9ca46b148a402a6d1ae6
chenaa083 <chenaa083@foxmail.com>
2023-12-23 17:32:23 +0800

    12.23

--- Readme of me.md
+++ Readme of me.md
@@ -1,2 +1,46 @@
 base of NeuroKit
 教程：https://neuropsychology.github.io/NeuroKit/examples/index.html
+心电图信号（ECG）特征：
+ECG_Raw: 原始的心电图信号。
+ECG_Clean: 经过滤波等处理后的清理后的心电图信号。
+ECG_Rate: 心率。
+ECG_Quality: 心电图信号的质量指标。
+ECG_R_Peaks: R 波峰的位置。
+ECG_P_Peaks: P 波峰的位置。
+ECG_P_Onsets: P 波的起始位置。
+ECG_P_Offsets: P 波的结束位置。
+ECG_Q_Peaks: Q 波峰的位置。
+ECG_R_Onsets: R 波的起始位置。
+ECG_R_Offsets: R 波的结束位置。
+ECG_S_Peaks: S 波峰的位置。
+ECG_T_Peaks: T 波峰的位置。
+ECG_T_Onsets: T 波的起始位置。
+ECG_T_Offsets: T 波的结束位置。
+ECG_Phase_Atrial: 心房相位。
+ECG_Phase_Completion_Atrial: 心房相位完成度。
+ECG_Phase_Ventricular: 心室相位。
+ECG_Phase_Completion_Ventricular: 心室相位完成度。
+呼吸信号（RSP）特征：
+RSP_Raw: 原始的呼吸信号。
+RSP_Clean: 经过滤波等处理后的清理后的呼吸信号。
+RSP_Amplitude: 呼吸信号振幅。
+RSP_Rate: 呼吸率。
+RSP_RVT: 呼吸周期。
+RSP_Phase: 呼吸相位。
+RSP_Phase_Completion: 呼吸相位完成度。
+RSP_Symmetry_PeakTrough: 呼吸信号峰-谷对称性。
+RSP_Symmetry_RiseDecay: 呼吸信号上升-下降对称性。
+RSP_Peaks: 呼吸信号峰的位置。
+RSP_Troughs: 呼吸信号谷的位置。
+皮肤电活动（EDA）特征：
+EDA_Raw: 原始的皮肤电活动信号。
+EDA_Clean: 经过滤波等处理后的清理后的皮肤电活动信号。
+EDA_Tonic: 皮肤电活动信号的基础成分。
+EDA_Phasic: 皮肤电活动信号的相位成分。
+SCR_Onsets: 皮肤电活动信号的SCR（皮肤电反应）起始位置。
+SCR_Peaks: 皮肤电活动信号的SCR峰的位置。
+SCR_Height: 皮肤电活动信号的SCR峰高度。
+SCR_Amplitude: 皮肤电活动信号的SCR振幅。
+SCR_RiseTime: 皮肤电活动信号的SCR上升时间。
+SCR_Recovery: 皮肤电活动信号的SCR恢复幅度。
+SCR_RecoveryTime: 皮肤电活动信号的SCR恢复时间。
--- test.py
+++ test.py
@@ -11,28 +11,50 @@ data = nk.data("bio_eventrelated_100hz")
 # 对生物信号数据进行预处理。具体而言，
 # 对心电图（ECG）、呼吸信号（RSP），以及皮肤电活动（EDA）
 # 进行了滤波、寻找峰值等处理。处理后的数据和相关信息存储在
-# processed_data 和 info 变量中
+# processed_data 和 info 变量中,以ecg为例
 processed_data, info = nk.bio_process(ecg=data["ECG"], sampling_rate=100)
 
-# 使用处理过的数据计算相关的特征。这可能包括心率、
+# 使用处理过的数据计算相关的特征。包括心率、
 # 呼吸率、皮肤电活动的振幅等。结果存储在 results 变量中。
 results = nk.bio_analyze(processed_data, sampling_rate=100)
 
 # 下面展示结果
 # Plot ECG signal
-plt.figure(figsize=(12, 4))
-plt.subplot(2, 1, 1)
-plt.plot(data["ECG"])
+xlim = 500
+
+plt.figure(figsize=(8, 6))
+plt.subplot(1, 1, 1)
+plt.plot(data["ECG"],color='red')
+plt.ylim(-1.5, 1.5)
+# 设置 x 轴范围为 0 到 2000
+plt.xlim(0, xlim)
 plt.title('ECG Signal')
 plt.xlabel('Sample')
 plt.ylabel('Amplitude')
+
 # Plot processed ECG signal
-plt.subplot(2, 1, 2)
+plt.subplot(1, 1, 1)
 plt.plot(processed_data["ECG_Clean"])
-plt.title('Processed ECG Signal')
+
+plt.ylim(-1.5, 1.5)
+# 设置 x 轴范围为 0 到 2000
+plt.xlim(0, xlim)
+plt.title('ECG_Clean')
 plt.xlabel('Sample')
 plt.ylabel('Amplitude')
 
+# Plot processed ECG signal
+plt.subplot(1, 1, 1)
+# 获取 R 峰的位置
+r_peak_indices = [i for i, value in enumerate(processed_data["ECG_R_Peaks"]) if value == 1]
+# 绘制散点图
+plt.scatter(r_peak_indices, [1] * len(r_peak_indices), marker='o', color='red')
+plt.ylim(-1.5, 1.5)
+# 设置 x 轴范围为 0 到 2000
+plt.xlim(0, xlim)
+plt.title('R peaks')
+plt.xlabel('Sample')
+plt.ylabel('Amplitude')
 # Show the plots
 plt.tight_layout()
 plt.show()
